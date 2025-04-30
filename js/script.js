d588ca0333e2fb88c4151c8b4d0b4b9ed6182744
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-04-30 17:29:20 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  //input
+  const aLength = parseFloat(document.getElementById("a-length").value)
+  const bLength = parseFloat(document.getElementById("b-length").value)
+  const cLength = parseFloat(document.getElementById("c-length").value)
+  //angle process
+  const angleA = Math.acos((bLength**2 + cLength**2 - aLength**2) / (2 * bLength * cLength)) * (180/Math.PI)
+  const angleB = Math.acos((cLength**2 + aLength**2 - bLength**2) / (2 * cLength * aLength)) * (180/Math.PI)
+  const angleC = Math.acos((aLength**2 + bLength**2 - cLength**2) / (2 * aLength * bLength)) * (180/Math.PI)
+  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+  //main process
+  if (sumOfAngles != 180) {
+    document.getElementById("answer") = 
+      "<p>This isn't a triangle</p>"
+  }
+  else if (aLength == bLength && aLength == cLength) {
+    document.getElementById("answer") = 
+      "<p>This is an equilateral triangle</p>"
+  }
+  else if ((aLength == cLength || aLength || bLength) || bLength == cLength) {
+    document.getElementById("answer") = 
+      "<p>This is an isoceles triangle</p>"
+  }
+  else {
+    document.getElementById("answer") = 
+      "<p>This is a scalene triangle</p>"
+  }
+}
